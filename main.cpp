35a572a310b1ccf8eafd4bbf42934d50799a2a32
alwinunsrat <asambul@unsrat.ac.id>
2026-08-26 21:28:22 +0800

    Implement prime number check in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 
 using namespace std;
 
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int n;
-    if (!(cin >> n)) return 0;
-
-    // TODO: Lengkapi logika pengecekan bilangan prima
-    // Cetak "PRIMA" jika prima, atau "BUKAN PRIMA" jika bukan.
-    cout << "BELUM DIKERJAKAN" << endl;
-
+    if (cin >> n) {
+        if (isPrime(n)) {
+            cout << "PRIMA" << endl;
+        } else {
+            cout << "BUKAN PRIMA" << endl;
+        }
+    }
     return 0;
 }
